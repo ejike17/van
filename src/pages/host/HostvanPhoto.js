64fc8127a029e9bc18927aa7ce5bfb595da3a0ca
Ejike17 <ejikeannumba17@gmail.com>
2023-05-16 17:14:46 +0100

    Third commit for react router course

--- src/pages/host/HostvanPhoto.js
+++ src/pages/host/HostvanPhoto.js
@@ -2,9 +2,10 @@ import React from "react";
 import { useOutletContext } from "react-router-dom";
 
 const HostvanPhoto = () => {
-
-  const {van} = useOutletContext()
-  return <img src={van.imageUrl} className="host-van-detail-image"/>;
+  const { van } = useOutletContext();
+  return (
+    <img src={van.imageUrl} className="host-van-detail-image" alt="Van-Img" />
+  );
 };
 
 export default HostvanPhoto;
